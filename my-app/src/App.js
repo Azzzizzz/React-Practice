8b8beae6189f47189b4396d60ba404d70eebe3f1
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-10-04 14:51:33 +0530

    Initial Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+
+
+
+function Counter() {
+  let count = 0
+  return (
+    <div>
+      <h1>Count - {count}</h1>
+    <button >++</button>
+    </div>
+
+  )
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello World</h1>
+        <Counter/>
+    </div>
+    </div>
+  );
+ 
+}
+
+export default App;
