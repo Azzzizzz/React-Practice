28c1fa2272dd7bcbe4264e7494ffe91d492ca892
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-10-11 21:38:30 +0530

    Added Bootstrap and decremnt button

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -6,12 +6,21 @@ class Counter extends React.Component {
   onBtnClicked = () => {
     this.setState({ count: ++this.state.count });
   };
+
+  dec = () => {
+    this.setState({ count: --this.state.count });
+  };
   render() {
     return (
       <div>
         <h1>Hello React From Class Component</h1>
         <h2>Count:{this.state.count}</h2>
-        <button onClick={this.onBtnClicked}>++</button>
+        <button className="btn  btn-primary" onClick={this.onBtnClicked}>
+          ++
+        </button>
+        <button className="btn btn-danger" onClick={this.dec}>
+          --
+        </button>
       </div>
     );
   }
