f95b69b4f6dda456bce7ff7e3351cfbb4f785316
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-10-13 21:01:39 +0530

    Header, Navbar, routing, Footer

--- React v2/github-users-display/src/App.js
+++ React v2/github-users-display/src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Users from "./Users";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NotFound from "./NotFound";
 
 export default function App() {
   return (
     <div>
-      <Users />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/users" component={Users} />
+          {/* If user enters gives invalid url */}
+          {/* <Redirect to="/" />  */}
+          <Route path="**" component={NotFound} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+
+      {/* <Users /> */}
     </div>
   );
 }
